3ce7e57e1c4e4f433f3bbe5b7aece7478823a571
Andrej Zlatev <andrej.zlatev31@gmail.com>
2024-02-05 17:41:33 +0100

    Implement solution for exercise 3

--- workshop/Javascript - Array Methods 1/3/main.js
+++ workshop/Javascript - Array Methods 1/3/main.js
@@ -4,3 +4,18 @@
 // - Find the position of a given element.
 // - Print to the console, the element after the one you found the position of.
 // - Otherwise, print to the user that the fruit was not found.
+
+const array = ["apple", "cherry", "banana", "date", "fig"];
+const userInput = prompt("Enter fruit name").toUpperCase();
+
+function findPosition(index, arr) {
+  const indexNumber = arr.findIndex(
+    (item) => item.toUpperCase() === index.toUpperCase()
+  );
+  return indexNumber !== -1
+    ? `Your fruit is ${indexNumber}`
+    : `Fruit not found`;
+}
+
+const result = findPosition(userInput, array);
+console.log(result);
