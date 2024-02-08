7bbc2414b8664ad501ed199e2718f8aeec476e2c
Andrej Zlatev <andrej.zlatev31@gmail.com>
2024-02-08 12:58:57 +0100

    Implement solution for exercise 1

---
+++ homework/Functions (advanced)/1/main.js
@@ -0,0 +1,31 @@
+// Exercise 1
+// 3
+// 1. Write a function that:
+// 1.1. Add two numbers and return the result
+// 1.2. Subtract two numbers and return the result
+// 1.3. Multiply two numbers and return the result
+// 1.4. Divide two numbers and return the result
+// 2. Define two number variables
+// 3. Invoke each of the four function on those two numbers
+// 4. Log the results in the console
+
+const subtract = (a, b) => {
+  return a + b;
+};
+
+result = subtract(5, 5);
+console.log(result);
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+result = multiply(5, 5);
+console.log(result);
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+result = divide(5, 5);
+console.log(result);
