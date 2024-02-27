47d6f5eb9ebe3af9152ee988a47f1a73d748eeb7
Andrej Zlatev <andrej.zlatev31@gmail.com>
2024-02-27 22:06:33 +0100

    Implementing solution for exercise 3

--- homework/Events (1)/1/main.js
+++ homework/Events (1)/1/main.js
@@ -24,3 +24,32 @@ const onMouseMove = () => btnEl.classList.toggle("hover");
 
 btnEl.addEventListener("mouseover", onMouseMove);
 btnEl.addEventListener("mouseout", onMouseMove);
+
+// Exercise III
+// 1. Add two more divs with a class of “row” to the document.
+// 2. In the first one, add a label with the text “Name” and an input tag next to the label.
+// 3. In the second one, add a label with the text “About me” and a textarea next to the label.
+// 4. Define two empty variables: name and aboutMe.
+// 5. Define a function called onNameChange which should simply rewrite the name variable with the
+// value from the input field.
+// 6. Define a function called onAboutChange which should rewrite the aboutMe variable with the
+// value from the textarea.
+// 7. Add a “keyup” event listener to both the input and the textarea, triggering onNameChange for
+// the input and onAboutChange for the textarea.
+
+const textAreaEl = document.querySelector("#aboutMe");
+const nameEl = document.querySelector("#name");
+let name = "";
+let aboutMe = "";
+
+const onNameChange = () => {
+  name = nameEl.value;
+  console.log(name);
+};
+
+const onAboutChange = () => {
+  aboutMe = textAreaEl.value;
+  console.log(aboutMe);
+};
+nameEl.addEventListener("keyup", onNameChange);
+textAreaEl.addEventListener("keyup", onAboutChange);
