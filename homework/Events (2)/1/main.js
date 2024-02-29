5a4f1bc4d06da77c57222cc2c7be57354b3bf4fb
Andrej Zlatev <andrej.zlatev31@gmail.com>
2024-02-29 21:28:52 +0100

    Implementing solution for exercise 1

---
+++ homework/Events (2)/1/main.js
@@ -0,0 +1,26 @@
+// function onLoad() {
+//   alert("Welcome!");
+// }
+// function onUnload(event) {
+//   // this is the only way this event works, alerts/messages are ignored.
+//   event.returnValue = "Are you sure you want to leave this page?";
+//   return true;
+// }
+// window.addEventListener("load", onLoad);
+// window.addEventListener("beforeunload", onUnload);
+
+// Exercise I
+// 1. In CSS, give the body height 10000vh.
+// 2. Define a function onScroll and attach it on a ‘scroll’ event on the window.
+// 3. Inside the ‘scroll’ event you have access to the window.scrollY property which gives you the exact
+// position of the page you are at.
+// 4. Using the window.scrollY check if you have scrolled:
+// a) past 1000px on the page, and if you had, color the body red.
+// b) otherwise, color the body green.
+
+const onScroll = () => {
+  scrollY > 1000
+    ? (document.body.style.backgroundColor = "red")
+    : (document.body.style.backgroundColor = "green");
+};
+window.addEventListener("scroll", onScroll);
