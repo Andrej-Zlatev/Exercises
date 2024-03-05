b80d59f803e4a775a32d54297bf4f2ebdfcde93e
Andrej Zlatev <andrej.zlatev31@gmail.com>
2024-03-05 08:30:59 +0100

    Implementing solution for exercise 1

---
+++ homework/Objects 1/1/main.js
@@ -0,0 +1,11 @@
+// Exercise 1
+// 1. Define an object called “Cat”.
+// 2. The “Cat” object should also contain two variables (properties) called “name” and “color”. Set some
+// values to them (for example: “Tom” and “blue”).
+
+const cat = {
+  name: "Tom",
+  color: "Blue",
+};
+
+console.log(cat.name, cat.color);
