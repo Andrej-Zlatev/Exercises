50318ba60ffadc131786f4c262adca7f7894b87d
Andrej Zlatev <andrej.zlatev31@gmail.com>
2024-03-16 12:01:39 +0100

    Implementing solution for exercise 4

---
+++ workshop/Events 3 and Objects 1/4/main.js
@@ -0,0 +1,4 @@
+// Exercise 04
+// - There is a html starter file for this exercise.
+// - Make a JavaScript function that logs all keyboard events.
+// - Display the last pressed key’s character and its keycode in the page.
