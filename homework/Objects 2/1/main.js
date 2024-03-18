41429643accff9e917f2613af5f20d40364b05f8
Andrej Zlatev <andrej.zlatev31@gmail.com>
2024-03-18 23:45:55 +0100

    Implementing solution for exercise 2

--- homework/Objects 2/1/main.js
+++ homework/Objects 2/1/main.js
@@ -1,8 +1,28 @@
-function Cat(name, color) {
-  this.name = name;
-  this.color = color;
+// function Cat(name, color) {
+//   this.name = name;
+//   this.color = color;
+// }
+
+// const newCat = new Cat("Jack", "Black");
+
+// console.log(newCat);
+
+function Cat() {
+  this.name = "";
+  this.color = "";
+  this.setName = function (name) {
+    this.name = name;
+  };
+  this.setColor = function (color) {
+    this.color = color;
+  };
+  this.SayNameAndColor = function () {
+    alert(`The cat name is ${this.name} and the color is ${this.color}`);
+  };
 }
 
-const newCat = new Cat("Jack", "Black");
-
-console.log(newCat);
+const catOne = new Cat();
+catOne.setName("Tom");
+catOne.setColor("Blue");
+catOne.SayNameAndColor();
+console.log(catOne);
