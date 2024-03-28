c8aa6f50cd2be41651bc1851a597bf6bad555428
Andrej Zlatev <andrej.zlatev31@gmail.com>
2024-03-28 18:35:44 +0100

    Implementing solution for exercise 3

--- homework/Objects(4) - Inheritance/1/main.js
+++ homework/Objects(4) - Inheritance/1/main.js
@@ -8,5 +8,18 @@ Person.prototype.sayFullName = function () {
   alert(`Firstname: ${this.firstName} Lastname: ${this.lastName}`);
 };
 
-newPerson.sayFullName();
-// console.log(newPerson);
+// newPerson.sayFullName();
+
+function Programmer(firstName, lastName, favLanguage) {
+  Person.call(this, firstName, lastName);
+  this.favLanguage = favLanguage;
+}
+
+Programmer.prototype = Object.create(Person.prototype);
+Programmer.prototype.constructor = Programmer;
+Programmer.prototype.sayFavoriteLanguage = function () {
+  alert(this.favLanguage);
+};
+const newProgrammer = new Programmer("Jane", "Doe", "Javascript");
+newProgrammer.sayFullName();
+newProgrammer.sayFavoriteLanguage();
