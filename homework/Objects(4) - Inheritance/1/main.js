43f874e1ab67eb7a184d7fb462c3ae116d13ba9f
Andrej Zlatev <andrej.zlatev31@gmail.com>
2024-03-28 19:23:48 +0100

    Implementing solution for exercise 4

--- homework/Objects(4) - Inheritance/1/main.js
+++ homework/Objects(4) - Inheritance/1/main.js
@@ -1,25 +1,49 @@
-function Person(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
+// function Person(firstName, lastName) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+// }
+
+// const newPerson = new Person("John", "Doe");
+// Person.prototype.sayFullName = function () {
+//   alert(`Firstname: ${this.firstName} Lastname: ${this.lastName}`);
+// };
+
+// // newPerson.sayFullName();
+
+// function Programmer(firstName, lastName, favLanguage) {
+//   Person.call(this, firstName, lastName);
+//   this.favLanguage = favLanguage;
+// }
+
+// Programmer.prototype = Object.create(Person.prototype);
+// Programmer.prototype.constructor = Programmer;
+// Programmer.prototype.sayFavoriteLanguage = function () {
+//   alert(this.favLanguage);
+// };
+// const newProgrammer = new Programmer("Jane", "Doe", "Javascript");
+// newProgrammer.sayFullName();
+// newProgrammer.sayFavoriteLanguage();
+
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  sayFullName() {
+    alert(`Full name: ${this.firstName} ${this.lastName}`);
+  }
 }
 
-const newPerson = new Person("John", "Doe");
-Person.prototype.sayFullName = function () {
-  alert(`Firstname: ${this.firstName} Lastname: ${this.lastName}`);
-};
-
-// newPerson.sayFullName();
-
-function Programmer(firstName, lastName, favLanguage) {
-  Person.call(this, firstName, lastName);
-  this.favLanguage = favLanguage;
+class Programmer extends Person {
+  constructor(firstName, lastName, favLanguage) {
+    super(firstName, lastName);
+    this.favLanguage = favLanguage;
+  }
+  sayFavoriteLanguage() {
+    alert(`Favorite language: ${this.favLanguage}`);
+  }
 }
 
-Programmer.prototype = Object.create(Person.prototype);
-Programmer.prototype.constructor = Programmer;
-Programmer.prototype.sayFavoriteLanguage = function () {
-  alert(this.favLanguage);
-};
-const newProgrammer = new Programmer("Jane", "Doe", "Javascript");
+const newProgrammer = new Programmer("John", "Doe", "Javascript");
 newProgrammer.sayFullName();
 newProgrammer.sayFavoriteLanguage();
