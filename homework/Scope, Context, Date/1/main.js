22632c2fd3237c676924c2c614a0f647040af9ad
Andrej Zlatev <andrej.zlatev31@gmail.com>
2024-03-31 15:33:07 +0200

    Implementing solution for exercise 2

--- homework/Scope, Context, Date/1/main.js
+++ homework/Scope, Context, Date/1/main.js
@@ -1,23 +1,33 @@
 const date = new Date();
 
-const getCurrentMonth = (currentMonth) => {
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+// const getCurrentMonth = (currentMonth) => {
+//   const months = [
+//     "January",
+//     "February",
+//     "March",
+//     "April",
+//     "May",
+//     "June",
+//     "July",
+//     "August",
+//     "September",
+//     "October",
+//     "November",
+//     "December",
+//   ];
 
-  return months[currentMonth];
-};
+//   return months[currentMonth];
+// };
 
-const result = getCurrentMonth(date.getMonth());
-console.log(result);
+// const result = getCurrentMonth(date.getMonth());
+// console.log(result);
+
+function isWeekend(day) {
+  if (day === 6 || day === 0) {
+    console.log("It is a weekend");
+  } else {
+    console.log("It is not a weekend");
+  }
+}
+
+isWeekend(date.getDay());
