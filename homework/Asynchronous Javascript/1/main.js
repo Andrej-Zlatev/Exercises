2b2243f9738db4ce8f8c78a1f8f72294f69e57e9
Andrej Zlatev <andrej.zlatev31@gmail.com>
2024-04-07 21:30:01 +0200

    Solution

--- homework/Asynchronous Javascript/1/main.js
+++ homework/Asynchronous Javascript/1/main.js
@@ -1,14 +1,36 @@
-function test(id, cb) {
+// function test(id, cb) {
+//   setTimeout(() => {
+//     cb({
+//       id: id,
+//       name: "Andrej",
+//     });
+//   }, 2000);
+// }
+
+// function getUserProfile(username, cb) {
+//   setTimeout(() => {
+//     cb({ username: username, photo: "slika.jpg" });
+//   }, 1000);
+// }
+
+// function callBack(obj) {
+//   console.log("id", obj);
+//   getUserProfile(obj.obj, function (userProfile) {
+//     console.log("userProfile", userProfile);
+//   });
+// }
+
+// test(1, callBack);
+
+const test = new Promise((resolve, reject) => {
+  let num = 15;
   setTimeout(() => {
-    cb({
-      id: id,
-      name: "Andrej",
-    });
+    if (num > 10) {
+      resolve("The number is higher than 10");
+    } else {
+      reject("The number is lower than 10");
+    }
   }, 2000);
-}
+});
 
-function callBack(obj) {
-  console.log(obj);
-}
-
-test(1, callBack);
+test.then((res) => console.log(res)).catch((error) => console.log(error));
