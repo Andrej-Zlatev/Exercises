3c73328f87f6713bc0e7798cbf478a691445908b
Andrej Zlatev <andrej.zlatev31@gmail.com>
2024-06-02 16:00:17 +0200

    TS Exercise

--- homework/Typescript Basics/dist/index.js
+++ homework/Typescript Basics/dist/index.js
@@ -1,7 +1,34 @@
 "use strict";
-const logPlayer = (player) => {
-    console.log(player.name, player.num);
+// interface playerInfo {
+//   name: String;
+//   num: Number;
+// }
+// const logPlayer = (player: playerInfo) => {
+//   console.log(player.name, player.num);
+// };
+// logPlayer({ name: "Cristiano Ronaldo", num: 7 });
+// let num = 7;
+// num = "seven";
+// Exercise I
+// - Create two inputs in HTML and a button with text Add
+// - In JavaScript create a function that will accept two parameters and simply returns
+// the sum of those two parameters
+// - Select the inputs and the button in javascript
+// - Add event listener that will execute the add function on button click which as
+// parameters will take the values from the inputs
+// - Add the result of the function into the document
+const firstNum = document.querySelector("#firstNum");
+const secondNum = document.querySelector("#secondNum");
+const btn = document.querySelector("button");
+const sum = (a, b) => {
+  return a + b;
 };
-logPlayer({ name: "Cristiano Ronaldo", num: 7 });
-let num = 7;
-num = "seven";
+btn === null || btn === void 0
+  ? void 0
+  : btn.addEventListener("click", () => {
+      if (firstNum && secondNum) {
+        const result = sum(Number(firstNum.value), Number(secondNum.value));
+        console.log(result);
+      }
+    });
+console.log("test");
--- homework/Typescript Basics/src/index.ts
+++ homework/Typescript Basics/src/index.ts
@@ -1,12 +1,38 @@
-interface playerInfo {
-  name: String;
-  num: Number;
-}
+// interface playerInfo {
+//   name: String;
+//   num: Number;
+// }
 
-const logPlayer = (player: playerInfo) => {
-  console.log(player.name, player.num);
+// const logPlayer = (player: playerInfo) => {
+//   console.log(player.name, player.num);
+// };
+
+// logPlayer({ name: "Cristiano Ronaldo", num: 7 });
+// let num = 7;
+// num = "seven";
+
+// Exercise I
+// - Create two inputs in HTML and a button with text Add
+// - In JavaScript create a function that will accept two parameters and simply returns
+// the sum of those two parameters
+// - Select the inputs and the button in javascript
+// - Add event listener that will execute the add function on button click which as
+// parameters will take the values from the inputs
+// - Add the result of the function into the document
+
+const firstNum: HTMLInputElement | null = document.querySelector("#firstNum");
+const secondNum: HTMLInputElement | null = document.querySelector("#secondNum");
+const btn: HTMLButtonElement | null = document.querySelector("button");
+
+const sum = (a: number, b: number): number => {
+  return a + b;
 };
 
-logPlayer({ name: "Cristiano Ronaldo", num: 7 });
-let num = 7;
-num = "seven";
+btn?.addEventListener("click", () => {
+  if (firstNum && secondNum) {
+    const result = sum(Number(firstNum.value), Number(secondNum.value));
+    console.log(result);
+  }
+});
+
+console.log("test");
