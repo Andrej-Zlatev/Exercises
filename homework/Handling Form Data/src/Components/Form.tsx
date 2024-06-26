fc7bfeb400b0a369819fc2a2dde3dabb51cad9a7
Andrej Zlatev <andrej.zlatev31@gmail.com>
2024-06-26 16:19:07 +0200

    Exercise

--- homework/Handling Form Data/src/Components/Form.tsx
+++ homework/Handling Form Data/src/Components/Form.tsx
@@ -1,25 +1,47 @@
 import { useState } from "react";
 
 const Form = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [textArea, setTextArea] = useState("");
-  const [profession, setProfession] = useState("");
+  //   const [firstName, setFirstName] = useState("");
+  //   const [lastName, setLastName] = useState("");
+  //   const [textArea, setTextArea] = useState("");
+  //   const [profession, setProfession] = useState("");
+  //   const [lookingForAjob, setLookingForAjob] = useState(Boolean);
 
-  const handleFirstName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFirstName(e.target.value);
-  };
+  const [form, setForm] = useState({
+    firstName: "",
+    lastName: "",
+    textArea: "",
+    profession: "",
+    lookingForAjob: false,
+  });
 
-  const handleLastName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLastName(e.target.value);
-  };
+  //   const handleFirstName = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //     setFirstName(e.target.value);
+  //   };
 
-  const handleTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setTextArea(e.target.value);
-  };
+  //   const handleLastName = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //     setLastName(e.target.value);
+  //   };
 
-  const handleProfession = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setProfession(e.target.value);
+  //   const handleTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+  //     setTextArea(e.target.value);
+  //   };
+
+  //   const handleProfession = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  //     setProfession(e.target.value);
+  //   };
+
+  //   const handleLookingForAjob = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //     setLookingForAjob(e.target.checked);
+  //   };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleForm = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
   };
 
   return (
@@ -30,8 +52,9 @@ const Form = () => {
           <input
             type="text"
             id="firstName"
-            value={firstName}
-            onChange={handleFirstName}
+            name="firstName"
+            value={form.firstName}
+            onChange={handleForm}
           />
         </div>
         <div>
@@ -39,38 +62,43 @@ const Form = () => {
           <input
             type="text"
             id="lastName"
-            value={lastName}
-            onChange={handleLastName}
+            name="lastName"
+            value={form.lastName}
+            onChange={handleForm}
           />
         </div>
         <div>
           <label htmlFor="aboutMe">About Me</label>
           <textarea
-            name="About Me"
+            name="textArea"
             id="aboutMe"
-            onChange={handleTextArea}
-            value={textArea}
+            onChange={handleForm}
+            value={form.textArea}
           ></textarea>
         </div>
       </form>
       <div>
-        <p>First Name: {firstName}</p>
-        <p>Last Name {lastName}</p>
-        <p>About me: {textArea}</p>
-        <p>Profession: {profession}</p>
+        <p>First Name: {form.firstName}</p>
+        <p>Last Name {form.lastName}</p>
+        <p>About me: {form.textArea}</p>
+        <p>Profession: {form.profession}</p>
       </div>
       <div>
-        <select
-          name="professions"
-          id="professions"
-          value={profession}
-          onChange={handleProfession}
-        >
-          <option value="">Choose profession</option>
-          <option value="frontend-developer">frontend</option>
-          <option value="backend-developer">backend</option>
-          <option value="design-developer">design</option>
-        </select>
+        <label htmlFor="LookingForAjob">Looking for a job?</label>
+        <input type="checkbox" onChange={handleForm} name="lookingForAjob" />
+        {form.lookingForAjob && (
+          <select
+            name="profession"
+            id="professions"
+            value={form.profession}
+            onChange={handleForm}
+          >
+            <option value="">Choose profession</option>
+            <option value="frontend-developer">frontend</option>
+            <option value="backend-developer">backend</option>
+            <option value="design-developer">design</option>
+          </select>
+        )}
       </div>
     </div>
   );
